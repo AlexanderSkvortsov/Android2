13ffbf2c059278ed439de9d38581b9f47237a00d
Alexander <iskrasupport@mail.ru>
2020-04-13 18:41:43 +0300

    Lesson2 final

--- Lesson1/app/src/main/java/ru/geekbrains/lesson1/MainActivity.java
+++ Lesson1/app/src/main/java/ru/geekbrains/lesson1/MainActivity.java
@@ -2,13 +2,55 @@ package ru.geekbrains.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView newCityName;
+    TextView newPressure;
+    TextView newPressureName;
+    TextView newWind;
+    TextView newWindName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitView();
+        PullView();
     }
+
+    private void InitView()
+    {
+        newCityName = findViewById(R.id.cityName);
+        newPressure = findViewById(R.id.cityPressure);;
+        newPressureName = findViewById(R.id.txtPressureName);;
+        newWind = findViewById(R.id.cityWind);;
+        newWindName = findViewById(R.id.txtWindName);;
+    }
+
+    private void PullView()
+    {
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("CITY_FROM_LIST"); //if it's a string you stored.
+        if (value != null){
+            newCityName.setText(value);
+        }
+
+        boolean value1;
+        value1 = intent.getBooleanExtra("PRESSURE_SHOW", true); //if it's a string you stored.
+        newPressure.setVisibility((value1)? View.VISIBLE:View.INVISIBLE);
+        newPressureName.setVisibility((value1)? View.VISIBLE:View.INVISIBLE);
+
+        value1 = intent.getBooleanExtra("WIND_SHOW",true); //if it's a string you stored.
+        newWind.setVisibility((value1)? View.VISIBLE:View.INVISIBLE);
+        newWindName.setVisibility((value1)? View.VISIBLE:View.INVISIBLE);
+
+    }
+
 }
