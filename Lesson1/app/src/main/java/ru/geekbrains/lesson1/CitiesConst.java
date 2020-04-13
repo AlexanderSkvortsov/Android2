d4e93b6333272391748b551982b3b60fa0929b7c
Alexander <iskrasupport@mail.ru>
2020-04-14 00:35:31 +0300

    Lesson3 final

---
+++ Lesson1/app/src/main/java/ru/geekbrains/lesson1/CitiesConst.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.lesson1;
+
+public interface CitiesConst {
+    public static final String CITY_FROM_LIST = "CITY_FROM_LIST";
+    public static final String PRESSURE_SHOW = "PRESSURE_SHOW";
+    public static final String WIND_SHOW = "WIND_SHOW";
+    public static final String CITY_TEMPERATURE = "CITY_TEMPERATURE";
+}
